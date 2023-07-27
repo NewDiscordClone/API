25e5dfbe23e2db544ecd6a2b8ff7b4d1d8248da3
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2023-07-27 13:39:23 +0300

    Basic ChatHub

---
+++ WebApi/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApi.Hubs;
+
+public class ChatHub : Hub
+{
+    public void AddMessage(string message)
+    {
+        // Call the MessageAdded method to update clients.
+        Clients.All.SendAsync("MessageAdded", message);
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,23 @@
+using WebApi.Hubs;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+//builder.Services.AddSingleton<IChatService, ChatService>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            ;
+    });
+});
 WebApplication app = builder.Build();
+
+app.UseCors();
+app.MapHub<ChatHub>("chat");
 
 app.MapGet("/", () => "Hello World!");
 
