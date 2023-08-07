e9329421a12a1b426263ea9e808fa892cd1a0302
Grabtot <dneshotkin@gmail.com>
2023-08-07 19:00:47 +0300

    Create MediatR Request for AddMessage Add authorize atribute for #14

---
+++ Application/Messages/AddMessageRequest/AddMessageRequestHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using MediatR;
+
+namespace Application.Messages.AddMessageRequest
+{
+
+    public class AddMessageRequestHandler : IRequestHandler<AddMessageRequest, Message>
+    {
+        private readonly IAppDbContext _context;
+
+        public AddMessageRequestHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Message> Handle(AddMessageRequest request, CancellationToken cancellationToken)
+        {
+            PrivateChat? chat = await _context.PrivateChats
+                .FindAsync(new object[] { request.ChatId }, cancellationToken);
+            User? user = await _context.Users.FindAsync(new object[] { request.UserId }, cancellationToken);
+
+            if (chat is null || user is null)
+                throw new Exception("Chat or user not found");
+
+            Message message = new()
+            {
+                Text = request.Text,
+                Chat = chat,
+                SendTime = DateTime.UtcNow,
+                User = user
+            };
+
+            await _context.Messages.AddAsync(message, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return message;
+        }
+    }
+}
--- WebApi/Hubs/ChatHub.cs
+++ WebApi/Hubs/ChatHub.cs
@@ -1,8 +1,8 @@
 ﻿using Application.Interfaces;
+using Application.Messages.AddMessageRequest;
 using Application.Models;
-using DataAccess;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 using System.Security.Claims;
 
@@ -12,13 +12,22 @@ namespace Application.Hubs;
 public class ChatHub : Hub
 {
     private readonly IAppDbContext _dbContext;
-    public ChatHub(IAppDbContext dbContext)
+    private readonly ILogger<ChatHub> _logger;
+    private readonly IMediator _mediator;
+
+    private event Func<Message, PrivateChat, Task> OnMessageReceived;
+    public ChatHub(IAppDbContext dbContext, IMediator mediator, ILogger<ChatHub> logger)
     {
         _dbContext = dbContext;
+        _mediator = mediator;
+        _logger = logger;
+
+        OnMessageReceived += SendMessageToPrivateChat;
     }
 
     private static readonly Dictionary<int, HashSet<string>> _userConnections = new();
-    private int UserId => int.Parse(Context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+    private int UserId => int.Parse(Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value
+        ?? throw new Exception("User not authenticated"));
     public override async Task OnConnectedAsync()
     {
         if (!_userConnections.ContainsKey(UserId))
@@ -27,11 +36,11 @@ public class ChatHub : Hub
         }
 
         _userConnections[UserId].Add(Context.ConnectionId);
-
+        _logger.LogInformation($"User {UserId} connected");
         await base.OnConnectedAsync();
     }
 
-    public override async Task OnDisconnectedAsync(Exception exception)
+    public override async Task OnDisconnectedAsync(Exception? exception)
     {
         if (_userConnections.ContainsKey(UserId))
         {
@@ -39,43 +48,49 @@ public class ChatHub : Hub
             if (_userConnections[UserId].Count == 0)
             {
                 _userConnections.Remove(UserId);
+                _logger.LogInformation($"User {UserId} disconnected");
             }
         }
 
         await base.OnDisconnectedAsync(exception);
     }
-    public async void AddMessage(string text, int chatId)
+    public async Task AddMessage(string text, int chatId)
     {
         if (!_userConnections.ContainsKey(UserId))
         {
-            // User is not connected, handle accordingly
             return;
         }
-
-        PrivateChat? chat = _dbContext.PrivateChats.Find(chatId);
-
-        if (chat == null)
-        {
-            // Chat not found, handle accordingly
-            return;
-        }
-
-        Message message = new()
+        AddMessageRequest messageRequest = new()
         {
             Text = text,
-            SendTime = DateTime.UtcNow,
-            User = _dbContext.Users.Find(UserId),
-            Chat = chat
+            ChatId = chatId,
+            UserId = UserId
         };
-
-        _dbContext.Messages.Add(message);
-        await _dbContext.SaveChangesAsync();
-
-        foreach (string? connectionId in chat.Users
-                     .Where(user => _userConnections.ContainsKey(user.Id))
-                     .SelectMany(user => _userConnections[user.Id]))
+        try
         {
-            Clients.Client(connectionId).SendAsync("MessageAdded", message);
+            Message message = await _mediator.Send(messageRequest);
+            _logger.LogInformation($"User {UserId} sent message to chat {chatId}");
+
+            if (message.Chat is PrivateChat chat)
+            {
+                OnMessageReceived?.Invoke(message, chat);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error while user {UserId} tries send message to chat {chatId}");
+        }
+    }
+
+    private async Task SendMessageToPrivateChat(Message message, PrivateChat chat)
+    {
+        IEnumerable<string> connectedUsers = chat.Users
+            .Where(user => _userConnections.ContainsKey(user.Id))
+            .SelectMany(user => _userConnections[user.Id]);
+
+        foreach (string connectionId in connectedUsers)
+        {
+            await Clients.Client(connectionId).SendAsync("MessageAdded", message);
         }
     }
 }
