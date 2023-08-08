394cf9d4a6651a5c288985b7b3c03a60277b0ce7
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2023-08-08 16:14:48 +0300

    use GetPrivateChatUserLookUpDto

--- Application/RequestModels/GetPrivateChats/GetPrivateChatLookUpDto.cs
+++ Application/RequestModels/GetPrivateChats/GetPrivateChatLookUpDto.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Interfaces;
 using Application.Models;
+using Application.RequestModels.GetMessages;
 using AutoMapper;
 
 namespace Application.RequestModels.GetPrivateChats
@@ -9,12 +10,12 @@ namespace Application.RequestModels.GetPrivateChats
         public int Id { get; init; }
         public string? Image { get; init; }
         public string? Title { get; init; }
-        public List<string> UsersNames { get; init; } = new();
+        public List<GetPrivateChatUserLookUpDto> Users { get; init; } = new();
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<PrivateChat, GetPrivateChatLookUpDto>()
-                .ForMember(dto => dto.UsersNames,
+                .ForMember(dto => dto.Users,
                 opt => opt.MapFrom(chat => chat.Users
                 .Select(user => user.DisplayName)));
         }
