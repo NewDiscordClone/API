d28a1011b508da02b32bc32d0ed08377ee47dc83
Grabtot <dneshotkin@gmail.com>
2023-08-09 13:56:03 +0300

    Remove channel lookup from server lookup dto

--- Application/Queries/GetServers/GetServerLookUpDto.cs
+++ Application/Queries/GetServers/GetServerLookUpDto.cs
@@ -9,7 +9,6 @@ namespace Application.Queries.GetServer
         public int Id { get; init; }
         public string Title { get; init; }
         public string? Image { get; init; }
-        public List<GetServerChannelLookUpDto> Channels { get; init; } = new();
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Server, GetServerLookupDto>();
