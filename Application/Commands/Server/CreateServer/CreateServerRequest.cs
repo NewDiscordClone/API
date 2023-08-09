8b0dc32112378b64f57fca4bc4b236f647b54ec5
Grabtot <dneshotkin@gmail.com>
2023-08-09 13:25:53 +0300

    Add create server command #21

---
+++ Application/Commands/Server/CreateServer/CreateServerRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Commands.Server.CreateServer
+{
+    public record CreateServerRequest : IRequest<int>
+    {
+        [Required, MaxLength(255)]
+        public string Title { get; init; }
+
+        [DataType(DataType.ImageUrl)]
+        public string? Image { get; init; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "UserId must be greater than 0.")]
+        public int UserId { get; init; }
+    }
+}
---
+++ Application/Commands/Server/CreateServer/CreateServerRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Application.Models;
+using MediatR;
+
+namespace Application.Commands.Server.CreateServer
+{
+    public class CreateServerRequestHandler : IRequestHandler<CreateServerRequest, int>
+    {
+        private readonly IAppDbContext _context;
+
+        public CreateServerRequestHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateServerRequest request, CancellationToken cancellationToken)
+        {
+            User user = await _context.Users
+                .FindAsync(new object[] { request.UserId },
+                cancellationToken: cancellationToken)
+                ?? throw new EntityNotFoundException($"User {request.UserId} not found");
+
+            Models.Server server = new()
+            {
+                Title = request.Title,
+                Image = request.Image,
+            };
+            server.ServerProfiles.Add(new() { User = user, Server = server });
+
+            await _context.Servers.AddAsync(server);
+            await _context.SaveChangesAsync();
+            return server.Id;
+        }
+    }
+}
