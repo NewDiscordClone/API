d6fa33b36299065537e02748382b936b8a0b9b7a
Grabtot <dneshotkin@gmail.com>
2023-08-09 18:26:18 +0300

    Add update server command #24

---
+++ Application/Commands/Server/UpdateServer/UpdateServerRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Commands.Server.UpdateServer
+{
+    public record UpdateServerRequest : IRequest
+    {
+        [Required]
+        public int ServerId { get; init; }
+        public string? Title { get; init; }
+        public string? Image { get; init; }
+    }
+}
---
+++ Application/Commands/Server/UpdateServer/UpdateServerRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using MediatR;
+namespace Application.Commands.Server.UpdateServer
+{
+    public class UpdateServerRequestHandler : IRequestHandler<UpdateServerRequest>
+    {
+        private readonly IAppDbContext _context;
+
+        public UpdateServerRequestHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UpdateServerRequest request, CancellationToken cancellationToken)
+        {
+            Models.Server server = await _context.FindByIdAsync<Models.Server>
+                (request.ServerId, cancellationToken);
+
+            server.Title = request.Title ?? server.Title;
+            server.Image = request.Image ?? server.Image;
+
+            _context.Servers.Update(server);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
