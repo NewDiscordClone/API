4b646eef15e1560dce19f19640323696b2c8628d
Grabtot <dneshotkin@gmail.com>
2023-08-09 18:32:45 +0300

    Add delete server command #24

---
+++ Application/Commands/Server/DeleteServer/DeleteServerRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using MediatR;
+
+namespace Application.Commands.Server.DeleteServer
+{
+    public class DeleteServerRequestHandler : IRequestHandler<DeleteServerRequest>
+    {
+        private readonly IAppDbContext _context;
+
+        public DeleteServerRequestHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(DeleteServerRequest request, CancellationToken cancellationToken)
+        {
+            Models.Server server = await _context.FindByIdAsync<Models.Server>
+                (request.Id, cancellationToken);
+
+            _context.Servers.Remove(server);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
