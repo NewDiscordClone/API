c23cc590efeecce400c67bdc3a73906163200679
Grabtot <dneshotkin@gmail.com>
2023-08-19 15:58:32 +0300

    Add create private chat tests

---
+++ Tests/PrivateChats/Commands/CreatePrivateChatTests.cs
@@ -0,0 +1,35 @@
+﻿using Application.Commands.PrivateChat.CreatePrivateChat;
+using Application.Models;
+using Tests.Common;
+
+namespace Tests.PrivateChats.Commands
+{
+    public class CreatePrivateChatTests : TestQueryBase
+    {
+        [Fact]
+        public async Task Success()
+        {
+            //Arrange
+            int userId = TestDbContextFactory.UserAId;
+            List<int> userIdlist = new() { userId, TestDbContextFactory.UserDId };
+            const string title = "TestCreate";
+
+            SetAuthorizedUserId(userId);
+
+            CreatePrivateChatRequest request = new()
+            {
+                UsersId = userIdlist,
+                Title = title,
+            };
+            CreatePrivateChatRequestHandler handler = new(Context, UserProvider);
+
+            //Act
+            PrivateChat result = await handler.Handle(request, CancellationToken);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(title, result.Title);
+            Assert.True(result.Users.Select(user => user.Id).SequenceEqual(userIdlist));
+        }
+    }
+}
