143a737f9f38ccf028bf695432df38f9d2b268ec
Grabtot <dneshotkin@gmail.com>
2023-08-30 16:13:44 +0300

    Create filter #46

--- Application/Exceptions/EntityNotFoundException.cs
+++ Application/Exceptions/EntityNotFoundException.cs
@@ -2,8 +2,19 @@
 {
     public class EntityNotFoundException : Exception
     {
-        public EntityNotFoundException(string? message) : base(message)
+        public object Id { get; init; }
+        private string _message;
+        public override string Message => _message;
+        public EntityNotFoundException(object id) : base()
         {
+            Id = id;
+            _message = $"Entity {id} not found";
         }
+        public EntityNotFoundException(string message, object id) : base()
+        {
+            Id = id;
+            _message = message;
+        }
+
     }
 }
---
+++ WebApi/Attributes/ExceptionFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Attributes
+{
+    public class ExceptionFilterAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            try
+            {
+                await next();
+            }
+            catch (NoPermissionsException exception)
+            {
+                context.Result = new ForbidResult(exception.Message);
+            }
+            catch (InvalidOperationException)
+            {
+                context.Result = new BadRequestResult();
+            }
+            catch (EntityNotFoundException ex)
+            {
+                context.Result = new NotFoundObjectResult(ex.Id);
+            }
+        }
+    }
+}
