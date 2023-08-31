7ee910bdf67ed08f6054a79f60903f9bc4e8d0c7
Grabtot <dneshotkin@gmail.com>
2023-08-31 13:05:08 +0300

    Add exeption filter #46

---
+++ WebApi/Attributes/ExceptionFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Attributes
+{
+    public class ExceptionFilterAttribute : Microsoft.AspNetCore.Mvc.Filters.ExceptionFilterAttribute
+    {
+        public override async Task OnExceptionAsync(ExceptionContext context)
+        {
+            context.Result = context.Exception switch
+            {
+                NoPermissionsException noPermissionsException => new ForbidResult(noPermissionsException.Message),
+                InvalidOperationException or ArgumentException => new BadRequestResult(),
+                EntityNotFoundException entityNotFoundException => new NotFoundObjectResult(entityNotFoundException.Id),
+                _ => new BadRequestResult()
+            };
+
+            await base.OnExceptionAsync(context);
+        }
+    }
+}
