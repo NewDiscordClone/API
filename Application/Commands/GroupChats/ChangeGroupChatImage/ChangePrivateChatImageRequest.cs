91c16ae77466579ffb3a5c4ff31ca3728a79d122
Grabtot <dneshotkin@gmail.com>
2023-09-04 15:46:50 +0300

    Add group chats documentation

--- Application/Commands/GroupChats/ChangeGroupChatImage/ChangePrivateChatImageRequest.cs
+++ Application/Commands/GroupChats/ChangeGroupChatImage/ChangePrivateChatImageRequest.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel;
+﻿using MediatR;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using Application.Models;
-using MediatR;
-using MongoDB.Bson;
 
 namespace Application.Commands.GroupChats.ChangeGroupChatImage
 {
-    public class ChangeGroupChatImageRequest : IRequest
+    public record ChangeGroupChatImageRequest : IRequest
     {
+        /// <summary>
+        /// The unique identifier of the group chat for which to change the image
+        /// </summary>
         [Required]
         [StringLength(24, MinimumLength = 24)]
         [DefaultValue("5f95a3c3d0ddad0017ea9291")]
         public string ChatId { get; init; }
-        
+
+        /// <summary>
+        /// The URL of the new image for the group chat
+        /// </summary>
         [Required]
         [DataType(DataType.ImageUrl)]
         [DefaultValue("https://localhost:7060/api/media/5f95a3c3d0ddad0017ea9291")]
--- Application/Commands/GroupChats/RemoveGroupChatMember/RemoveGroupChatMemberRequest.cs
+++ Application/Commands/GroupChats/RemoveGroupChatMember/RemoveGroupChatMemberRequest.cs
@@ -1,22 +1,29 @@
-﻿using System.ComponentModel;
+﻿using MediatR;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using Application.Models;
-using MediatR;
-using MongoDB.Bson;
 
 namespace Application.Commands.GroupChats.RemoveGroupChatMember
 {
-    public class RemoveGroupChatMemberRequest : IRequest
+    public record RemoveGroupChatMemberRequest : IRequest
     {
+        /// <summary>
+        /// The unique identifier of the group chat from which to remove a member.
+        /// </summary>
         [Required]
         [StringLength(24, MinimumLength = 24)]
         [DefaultValue("5f95a3c3d0ddad0017ea9291")]
         public string ChatId { get; init; }
-        
+
+        /// <summary>
+        /// The unique identifier of the member to be removed from the group chat.
+        /// </summary>
         [Required]
         [DefaultValue(1)]
         public int MemberId { get; init; }
-        
+
+        /// <summary>
+        /// Indicates whether to remove the member silently without sending notifications. (Optional, default is false)
+        /// </summary>
         [DefaultValue(false)]
         public bool Silent { get; init; } = false;
     }
