81fa3d39ef513b2bf6a6e1b792499371facfed54
Grabtot <dneshotkin@gmail.com>
2023-09-04 14:22:24 +0300

    Add chammel models documentation #51

--- Application/Commands/Channels/RenameChannel/RenameChannelRequest.cs
+++ Application/Commands/Channels/RenameChannel/RenameChannelRequest.cs
@@ -1,16 +1,22 @@
-﻿using System.ComponentModel;
+﻿using MediatR;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using MediatR;
 
 namespace Application.Commands.Channels.RenameChannel
 {
-    public class RenameChannelRequest : IRequest
+    public record RenameChannelRequest : IRequest
     {
+        /// <summary>
+        /// Id of the channel to be renamed
+        /// </summary>
         [Required]
         [StringLength(24, MinimumLength = 24)]
         [DefaultValue("5f95a3c3d0ddad0017ea9291")]
         public string ChatId { get; init; }
-        
+
+        /// <summary>
+        /// New name of the channel
+        /// </summary>
         [Required]
         [MaxLength(100)]
         [DefaultValue("NewTitle")]
