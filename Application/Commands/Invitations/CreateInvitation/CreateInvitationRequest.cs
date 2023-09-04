ba5d0fc0797b0e470de990ec573125701654d8da
Grabtot <dneshotkin@gmail.com>
2023-09-04 16:09:39 +0300

    Add invitation documentation #51

--- Application/Commands/Invitations/CreateInvitation/CreateInvitationRequest.cs
+++ Application/Commands/Invitations/CreateInvitation/CreateInvitationRequest.cs
@@ -4,8 +4,19 @@ namespace Application.Commands.Invitations.CreateInvitation
 {
     public record CreateInvitationRequest : IRequest<string>
     {
+        /// <summary>
+        /// The unique identifier of the server for which to create an invitation.
+        /// </summary>
         public string ServerId { get; set; }
+
+        /// <summary>
+        /// Indicates whether to include user information in the invitation.
+        /// </summary>
         public bool IncludeUser { get; set; }
+
+        /// <summary>
+        /// The expiration time of the invitation. (Optional)
+        /// </summary>
         public DateTime? ExpireTime { get; set; }
     }
 }
--- Application/Queries/GetInvitationDetails/InvitationDetailsDto.cs
+++ Application/Queries/GetInvitationDetails/InvitationDetailsDto.cs
@@ -1,14 +1,27 @@
-﻿using Application.Interfaces;
-using Application.Models;
-using AutoMapper;
+﻿using Application.Models;
 
 namespace Application.Queries.GetInvitationDetails
 {
     public class InvitationDetailsDto
     {
+        /// <summary>
+        /// The unique identifier of the invitation.
+        /// </summary>
         public string Id { get; set; }
+
+        /// <summary>
+        /// The user associated with the invitation. (Optional)
+        /// </summary>
         public UserLookUp? User { get; set; }
+
+        /// <summary>
+        /// The server associated with the invitation.
+        /// </summary>
         public ServerLookupDto Server { get; set; }
+
+        /// <summary>
+        /// The expiration time of the invitation. (Optional)
+        /// </summary>
         public DateTime? ExpireTime { get; set; }
     }
 }
