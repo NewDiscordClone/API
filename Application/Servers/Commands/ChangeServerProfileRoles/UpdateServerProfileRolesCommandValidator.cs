07caf00fc4783bce432f369bf246d0783352cb71
Grabtot <dneshotkin@gmail.com>
2023-09-16 19:03:53 +0300

    Add first validators #62

---
+++ Application/Servers/Commands/ChangeServerProfileRoles/UpdateServerProfileRolesCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Sparkle.Application.Common.Validation;
+
+namespace Sparkle.Application.Servers.Commands.ChangeServerProfileRoles
+{
+    public class UpdateServerProfileRolesCommandValidator : AbstractValidator<UpdateServerProfileRolesCommand>
+    {
+        public UpdateServerProfileRolesCommandValidator()
+        {
+            RuleFor(x => x.ServerId).NotNull().IsObjectId();
+            RuleFor(x => x.Roles).NotNull();
+            RuleFor(x => x.UserId).NotNull();
+        }
+    }
+}
