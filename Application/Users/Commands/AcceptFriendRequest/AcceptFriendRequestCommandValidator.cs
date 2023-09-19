12fc557a1574f5dcb1ad652a670e6d8006276a61
Grabtot <dneshotkin@gmail.com>
2023-09-19 16:47:32 +0300

    Add user validation

---
+++ Application/Users/Commands/AcceptFriendRequest/AcceptFriendRequestCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Sparkle.Application.Users.Commands.AcceptFriendRequest
+{
+    public class AcceptFriendRequestCommandValidator : AbstractValidator<AcceptFriendRequestCommand>
+    {
+        public AcceptFriendRequestCommandValidator()
+        {
+            RuleFor(c => c.UserId).NotNull();
+        }
+    }
+}
---
+++ Application/Users/Commands/FriendRequest/CreateFriendRequestCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Sparkle.Application.Users.Commands.FriendRequest
+{
+    public class CreateFriendRequestCommandValidator : AbstractValidator<CreateFriendRequestCommand>
+    {
+        public CreateFriendRequestCommandValidator()
+        {
+            RuleFor(c => c.UserId).NotNull();
+        }
+    }
+}
