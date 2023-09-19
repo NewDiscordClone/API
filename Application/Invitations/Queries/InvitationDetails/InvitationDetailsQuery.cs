636fc0c1fbec853789654330e480e1218616ed0e
Grabtot <dneshotkin@gmail.com>
2023-09-19 15:51:24 +0300

    Add invitation validators

--- Application/Invitations/Queries/InvitationDetails/InvitationDetailsQuery.cs
+++ Application/Invitations/Queries/InvitationDetails/InvitationDetailsQuery.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace Sparkle.Application.Invitations.Queries.InvitationDetails
 {
@@ -9,8 +8,6 @@ namespace Sparkle.Application.Invitations.Queries.InvitationDetails
         /// <summary>
         /// The unique identifier of the invitation for which to retrieve details
         /// </summary>
-        [Required]
-        [StringLength(24, MinimumLength = 24)]
         [DefaultValue("5f95a3c3d0ddad0017ea9291")]
         public string InvitationId { get; init; }
     }
---
+++ Application/Invitations/Queries/InvitationDetails/InvitationDetailsQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Sparkle.Application.Common.Validation;
+
+namespace Sparkle.Application.Invitations.Queries.InvitationDetails
+{
+    public class InvitationDetailsQueryValidator : AbstractValidator<InvitationDetailsQuery>
+    {
+        public InvitationDetailsQueryValidator()
+        {
+            RuleFor(c => c.InvitationId).NotNull().IsObjectId();
+        }
+    }
+}
