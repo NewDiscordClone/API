72be6025588bc04adedc9802ff07c40f30ea8cdc
Grabtot <dneshotkin@gmail.com>
2023-09-19 15:51:24 +0300

    Add invitation validators

---
+++ Application/Invitations/Commands/CreateInvitation/CreateInvitationCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Sparkle.Application.Common.Constants;
+using Sparkle.Application.Common.Validation;
+
+namespace Sparkle.Application.Invitations.Commands.CreateInvitation
+{
+    public class CreateInvitationCommandValidator : AbstractValidator<CreateInvitationCommand>
+    {
+        public CreateInvitationCommandValidator()
+        {
+            RuleFor(c => c.ServerId).NotNull().IsObjectId();
+            RuleFor(c => c.ExpireTime).NotNull();
+            RuleFor(c => c.ExpireTime).GreaterThan(DateTime.UtcNow);
+            RuleFor(c => c.ExpireTime).LessThan(DateTime.UtcNow.AddDays(Constants.Server.MaxInvetationLiveDays));
+        }
+    }
+}
