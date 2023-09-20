afcedc98e61321891435609469e49d62b7189350
Grabtot <dneshotkin@gmail.com>
2023-09-20 19:08:25 +0300

    Add documentation

--- Contracts/Roles/RoleResponse.cs
+++ Contracts/Roles/RoleResponse.cs
@@ -1,11 +1,40 @@
-﻿namespace Sparkle.Contracts.Roles
+﻿using System.Security.Claims;
+
+namespace Sparkle.Contracts.Roles
 {
+    /// <summary>
+    /// Role details
+    /// </summary>
     public class RoleResponse
     {
+        /// <summary>
+        /// Id of the role
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// Non-unique name of the role
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Color of the role in hex format
+        /// </summary>
         public string Color { get; set; }
+
+        /// <summary>
+        /// Id of the server where the role is located
+        /// </summary>
         public string ServerId { get; set; }
+
+        /// <summary>
+        /// Priority that indicates how high the role is in the hierarchy
+        /// </summary>
         public int Priority { get; set; }
+
+        /// <summary>
+        /// Claims of the role
+        /// </summary>
+        public IEnumerable<Claim> Claims { get; set; }
     }
 }
--- WebApi/Controllers/RoleController.cs
+++ WebApi/Controllers/RoleController.cs
@@ -24,7 +24,17 @@ namespace Sparkle.WebApi.Controllers
         {
         }
 
+        /// <summary>
+        /// Returns all roles of the server
+        /// </summary>
+        /// <param name="serverId">Id of the server to get the roles</param>
+        /// <response code="200">OK. List of roles</response>
+        /// <response code="404">Not Found. The server is not found</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<ActionResult> GetRoles(string serverId)
         {
             List<Role> roles = await Mediator.Send(new ServerRolesListQuery { ServerId = serverId });
@@ -32,6 +42,12 @@ namespace Sparkle.WebApi.Controllers
             return Ok(response);
         }
 
+        /// <summary>
+        /// Returns the role details
+        /// </summary>
+        /// <param name="roleId">Id of the role</param>
+        /// <response code="200">OK. Role details</response>
+        /// <response code="404">Not Found. The role is not found</response>
         [HttpGet("{roleId}")]
         public async Task<ActionResult> GetRole(Guid roleId)
         {
@@ -61,8 +77,6 @@ namespace Sparkle.WebApi.Controllers
         /// <param name="roleId">Id of the role to delete</param>
         /// <response code="204">No Content. Operation is successful</response>
         /// <response code="404">Not Found. The role is not found</response>
-        /// <response code="401">Unauthorized. The client must be authorized to send this request</response>
-        /// <response code="403">Forbidden. The client has not permissions to perform this action</response>
         [HttpDelete("{roleId}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -74,6 +88,13 @@ namespace Sparkle.WebApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Updates the role
+        /// </summary>
+        /// <param name="roleId">Id of the role to update</param>
+        /// <param name="request">New role's data</param>
+        /// <response code="200">Ok. Operation is successful</response>
+        /// <response code="404">Not Found. The role is not found</response>
         [HttpPut("{roleId}")]
         public async Task<ActionResult> UpdateRole(Guid roleId, UpdateRoleRequest request)
         {
@@ -84,6 +105,15 @@ namespace Sparkle.WebApi.Controllers
             return Ok(response);
         }
 
+        /// <summary>
+        /// Partially updates the role
+        /// </summary>
+        /// <param name="roleId">Id of the role to update</param>
+        /// <param name="name">New role's name</param>
+        /// <param name="color">New role's color in hex format</param>
+        /// <param name="priority">New role's priority</param>
+        /// <response code="200">Ok. Operation is successful</response>
+        /// <response code="404">Not Found. The role is not found</response>
         [HttpPatch("{roleId}")]
         public async Task<ActionResult> PartialUpdateRole(Guid roleId, string? name, string? color, int? priority)
         {
@@ -111,6 +141,12 @@ namespace Sparkle.WebApi.Controllers
             return Ok(response);
         }
 
+        /// <summary>
+        /// Updates the permissions of the role
+        /// </summary>
+        /// <param name="roleId">Id of the role to update claims</param>
+        /// <param name="claims">New set of claims</param>
+        /// <returns></returns>
         [HttpPatch("{roleId}/claims")]
         public async Task<ActionResult> UpdateRoleClaims(Guid roleId, IEnumerable<Claim> claims)
         {
