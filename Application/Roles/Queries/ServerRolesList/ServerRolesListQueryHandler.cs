47f5a29012cdc0bea4a1ed5496f7e2e2c17df5de
Grabtot <dneshotkin@gmail.com>
2023-09-20 18:40:15 +0300

    Add role queries

---
+++ Application/Roles/Queries/ServerRolesList/ServerRolesListQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Sparkle.Application.Common.Interfaces;
+using Sparkle.Application.Models;
+
+namespace Sparkle.Application.Roles.Queries.ServerRolesList
+{
+    public class ServerRolesListQueryHandler : RequestHandlerBase, IRequestHandler<ServerRolesListQuery, List<Role>>
+    {
+        public ServerRolesListQueryHandler(IAppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Role>> Handle(ServerRolesListQuery query, CancellationToken cancellationToken)
+        {
+            Context.SetToken(cancellationToken);
+
+            List<Role> roles = await Context.SqlRoles.FilterAsync(role => role.ServerId == query.ServerId);
+            return roles;
+        }
+    }
+}
