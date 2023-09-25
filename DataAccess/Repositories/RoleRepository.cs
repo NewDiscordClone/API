623b84c5cb7d3d9c89f43554490b6e12afc141be
Grabtot <dneshotkin@gmail.com>
2023-09-25 14:55:02 +0300

    Implement role repository

---
+++ DataAccess/Repositories/RoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sparkle.Application.Common.Constants;
+using Sparkle.Application.Common.Interfaces.Repositories;
+using Sparkle.Application.Models;
+
+namespace Sparkle.DataAccess.Repositories
+{
+    internal class RoleRepository : SimpleSqlDbSet<Role>, IRoleRepository
+    {
+        public RoleRepository(AppDbContext context) : base(context, default)
+        {
+        }
+
+        public async Task<Role> GetServerMemberRoleAsync(string serverId)
+        {
+            return await DbSet
+                .SingleAsync(role => role.ServerId == serverId
+                && role.Name == Constants.ServerProfile.DefaultMemberRoleName);
+        }
+    }
+}
