406fc0f6d6e023749be8471b93c1a2b2bd9687b5
Grabtot <dneshotkin@gmail.com>
2023-10-20 14:26:17 +0300

    Add role events #87

---
+++ Application/Models/Events/Roles/RoleEvents.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace Sparkle.Application.Models.Events.Roles
+{
+    public record RoleCreatedEvent(Role Role) : INotification;
+    public record RoleUpdatedEvent(Role Role) : INotification;
+    public record RoleDeletedEvent(Role Role) : INotification;
+    public record RoleAssignedEvent(Role Role, Profile Profile) : INotification;
+    public record RoleUnassignedEvent(Role Role, Profile Profile) : INotification;
+
+}
