71051a4b66eeac2a0e455122dbba9a37c78ebf03
Grabtot <dneshotkin@gmail.com>
2023-10-22 12:23:06 +0300

    add server factory closes #98

---
+++ Application/Common/Factories/ServerFactory.cs
@@ -0,0 +1,58 @@
+﻿using Sparkle.Application.Common.Interfaces;
+using Sparkle.Application.Models;
+
+namespace Sparkle.Application.Common.Factories
+{
+
+    public class ServerFactory
+    {
+        private readonly IRoleFactory _roleFactory;
+
+        private readonly IAuthorizedUserProvider _userProvider;
+        public ServerFactory(IRoleFactory roleFactory, IAuthorizedUserProvider userProvider)
+        {
+            _roleFactory = roleFactory;
+            _userProvider = userProvider;
+        }
+
+        private (Server Server, ServerProfile Owner) BaseServer(string title, string? image)
+        {
+            List<Role> roles = _roleFactory.GetDefaultServerRoles();
+            (Role ownerRole, Role memberRole) = (roles[0], roles[1]);
+
+            Server server = new()
+            {
+                Title = title,
+                Image = image,
+                Roles = roles.ConvertAll(role => role.Id),
+            };
+
+            ServerProfile owner = new()
+            {
+                UserId = _userProvider.GetUserId(),
+                ServerId = server.Id,
+                Roles = { ownerRole }
+            };
+
+            server.Profiles.Add(owner.Id);
+
+            return (server, owner);
+        }
+
+        public (Server Server, ServerProfile Owner, Channel Channel) DefaultServer(string title, string? image)
+        {
+            (Server server, ServerProfile owner) = BaseServer(title, image);
+
+            Channel welcomeChannel = new()
+            {
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now,
+                ServerId = server.Id,
+                Profiles = { owner.Id },
+                Title = Constants.Constants.Channel.WelcomeChannelName
+            };
+
+            return (server, owner, welcomeChannel);
+        }
+    }
+}
--- Application/Servers/Commands/CreateServer/CreateServerCommandHandler.cs
+++ Application/Servers/Commands/CreateServer/CreateServerCommandHandler.cs
@@ -1,5 +1,5 @@
 ﻿using MediatR;
-using Sparkle.Application.Common.Constants;
+using Sparkle.Application.Common.Factories;
 using Sparkle.Application.Common.Interfaces;
 using Sparkle.Application.Common.Interfaces.Repositories;
 using Sparkle.Application.Models;
@@ -8,42 +8,29 @@ namespace Sparkle.Application.Servers.Commands.CreateServer
 {
     public class CreateServerCommandHandler : RequestHandlerBase, IRequestHandler<CreateServerCommand, Server>
     {
-        private readonly IRoleFactory _roleFactory;
+        private readonly ServerFactory _serverFactory;
         private readonly IServerProfileRepository _serverProfileRepository;
         public async Task<Server> Handle(CreateServerCommand command, CancellationToken cancellationToken)
         {
             Context.SetToken(cancellationToken);
 
-            Server server = new()
-            {
-                Title = command.Title,
-                Image = command.Image,
-            };
-
-            List<Role> roles = _roleFactory.GetDefaultServerRoles();
-
-            Role ownerRole = roles.First(role => role.Name == Constants.Roles.ServerOwnerName);
-
-            ServerProfile owner = new()
-            {
-                UserId = UserId,
-                ServerId = server.Id,
-                Roles = { ownerRole }
-            };
-
-            server.Roles.AddRange(roles.ConvertAll(role => role.Id));
-            server.Profiles.Add(owner.Id);
+            (Server server, ServerProfile owner, Channel welcomeChannel) =
+                _serverFactory.DefaultServer(command.Title, command.Image);
 
             await _serverProfileRepository.AddAsync(owner, cancellationToken);
             await Context.Servers.AddAsync(server, cancellationToken);
+            await Context.Channels.AddAsync(welcomeChannel, cancellationToken);
 
             return server;
         }
 
-        public CreateServerCommandHandler(IAppDbContext context, IAuthorizedUserProvider userProvider, IRoleFactory roleFactory, IServerProfileRepository serverProfileRepository)
+        public CreateServerCommandHandler(IAppDbContext context,
+            IAuthorizedUserProvider userProvider,
+            ServerFactory serverFactory,
+            IServerProfileRepository serverProfileRepository)
             : base(context, userProvider)
         {
-            _roleFactory = roleFactory;
+            _serverFactory = serverFactory;
             _serverProfileRepository = serverProfileRepository;
         }
     }
