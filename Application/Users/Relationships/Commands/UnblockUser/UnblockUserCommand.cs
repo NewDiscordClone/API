d80f1a4615ba765a536e9d7519750f0a579a83f6
Grabtot <dneshotkin@gmail.com>
2023-11-03 14:16:50 +0200

    created unblock user endpoint #143

---
+++ Application/Users/Relationships/Commands/UnblockUser/UnblockUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Sparkle.Application.Models;
+
+namespace Sparkle.Application.Users.Relationships.Commands
+{
+    public record UnblockUserCommand(Guid UserId) : IRequest<Relationship>
+    {
+    }
+}
