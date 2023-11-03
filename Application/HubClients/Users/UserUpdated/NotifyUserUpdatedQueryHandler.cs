88b13f3dfccc99675a34a53a69456e0e27a386f7
Grabtot <dneshotkin@gmail.com>
2023-11-03 14:54:00 +0200

    fixes #144

--- Application/HubClients/Users/UserUpdated/NotifyUserUpdatedQueryHandler.cs
+++ Application/HubClients/Users/UserUpdated/NotifyUserUpdatedQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Sparkle.Application.Common.Interfaces;
 using Sparkle.Application.Models;
 using Sparkle.Application.Models.LookUps;
@@ -16,10 +17,35 @@ namespace Sparkle.Application.HubClients.Users.UserUpdated
 
         public async Task Handle(NotifyUserUpdatedQuery query, CancellationToken cancellationToken)
         {
-            User user = query.UpdatedUser;
-            UserViewModel notifyArg = Mapper.Map<UserViewModel>(user);
+            List<UserProfile> profiles = await Context.Users
+                .Where(user => user.Id == query.UpdatedUser.Id)
+                .SelectMany(user => user.UserProfiles)
+                .ToListAsync(cancellationToken);
+
+            List<Chat> chats = await Context.Chats
+                .FilterAsync(chat => profiles.Any(profile =>
+                profile.ChatId == chat.Id), cancellationToken);
+
+            List<string> connections = new();
+
+            foreach (Chat chat in chats)
+            {
+                connections.AddRange(GetConnections(chat));
+            }
+
+            List<Server> servers = await Context.Servers
+                .FilterAsync(server => profiles.OfType<ServerProfile>()
+                .Any(profile => profile.ServerId == server.Id), cancellationToken);
+
+            foreach (Server server in servers)
+            {
+                connections.AddRange(GetConnections(server));
+            }
+
+            UserViewModel notifyArg = Mapper.Map<UserViewModel>(profiles);
 
             await SendAsync(ClientMethods.UserUpdated, notifyArg, GetConnections(UserId));
+            await SendAsync(ClientMethods.UserUpdated, notifyArg, connections);
 
         }
     }
