a94af1b4a3c8628640fef61b3e55d6c40bddf960
Grabtot <dneshotkin@gmail.com>
2023-11-17 15:05:41 +0200

    add range role priority update endpoint

---
+++ Application/Servers/Roles/Commands/ChangeRangePriority/ChangeRangePriorityCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Sparkle.Application.Models;
+
+namespace Sparkle.Application.Servers.Roles.Commands.ChangeRangePriority
+{
+    public record ChangeRangePriorityCommand(Dictionary<Guid, int> Priorities)
+        : IRequest<IEnumerable<Role>>;
+}
---
+++ Application/Servers/Roles/Commands/ChangeRangePriority/ChangeRangePriorityCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Sparkle.Application.Common.Interfaces.Repositories;
+using Sparkle.Application.Models;
+
+namespace Sparkle.Application.Servers.Roles.Commands.ChangeRangePriority
+{
+    public class ChangeRangePriorityCommandHandler : IRequestHandler<ChangeRangePriorityCommand, IEnumerable<Role>>
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public ChangeRangePriorityCommandHandler(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<IEnumerable<Role>> Handle(ChangeRangePriorityCommand command, CancellationToken cancellationToken)
+        {
+            List<Role> roles = await _roleRepository.FilterAsync(role =>
+                command.Priorities.Keys.Contains(role.Id), cancellationToken);
+
+            foreach (Role role in roles)
+            {
+                role.Priority = command.Priorities[role.Id];
+                await _roleRepository.UpdateAsync(role, cancellationToken);
+            }
+
+            return roles;
+        }
+    }
+}
