d326755a559ed1ece2d72e68ad40550e672d7259
Grabtot <dneshotkin@gmail.com>
2023-11-20 18:09:13 +0200

    fixed #149

---
+++ Application/HubClients/Servers/ServerDeleted/ServerDeletedNotificationHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Sparkle.Application.Common.Interfaces;
+
+namespace Sparkle.Application.HubClients.Servers.ServerDeleted
+{
+    public class ServerDeletedNotificationHandler : HubRequestHandlerBase, INotificationHandler<NotifyServerDeletedEvent>
+    {
+        public ServerDeletedNotificationHandler(IHubContextProvider hubContextProvider, IAppDbContext context) : base(hubContextProvider, context)
+        {
+        }
+
+        public async Task Handle(NotifyServerDeletedEvent query, CancellationToken cancellationToken)
+        {
+            SetToken(cancellationToken);
+
+            await SendAsync(ClientMethods.ServerDeleted, query.Server.Id, GetConnections(query.UserIds));
+        }
+    }
+}
--- Application/Servers/Commands/DeleteServer/DeleteServerRequestHandler.cs
+++ Application/Servers/Commands/DeleteServer/DeleteServerRequestHandler.cs
@@ -1,11 +1,12 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Sparkle.Application.Common.Interfaces;
 using Sparkle.Application.Common.Interfaces.Repositories;
 using Sparkle.Application.Models;
 
 namespace Sparkle.Application.Servers.Commands.DeleteServer
 {
-    public class DeleteServerRequestHandler : RequestHandlerBase, IRequestHandler<DeleteServerCommand, Server>
+    public class DeleteServerRequestHandler : RequestHandlerBase, IRequestHandler<DeleteServerCommand, (Server Server, IEnumerable<Guid> UserIds)>
     {
         private readonly IRoleRepository _roleRepository;
         private readonly IServerProfileRepository _serverProfileRepository;
@@ -15,18 +16,24 @@ namespace Sparkle.Application.Servers.Commands.DeleteServer
             return Context;
         }
 
-        public async Task<Server> Handle(DeleteServerCommand request, CancellationToken cancellationToken)
+        public async Task<(Server Server, IEnumerable<Guid> UserIds)> Handle(DeleteServerCommand request, CancellationToken cancellationToken)
         {
             Context.SetToken(cancellationToken);
 
             Server server = await Context.Servers.FindAsync(request.ServerId, cancellationToken);
+            Guid[] userIds = await Context.UserProfiles
+                .OfType<ServerProfile>()
+                .Where(profile => profile.ServerId == server.Id)
+                .Select(profile => profile.UserId)
+                .ToArrayAsync();
+
 
             await _roleRepository.DeleteManyAsync(role => role.ServerId == request.ServerId, cancellationToken);
             await _serverProfileRepository.DeleteManyAsync(profile => profile.ServerId == request.ServerId, cancellationToken);
             await Context.Servers.DeleteAsync(server, cancellationToken);
             await Context.Channels.DeleteManyAsync(c => c.ServerId == request.ServerId, cancellationToken);
 
-            return server;
+            return (server, userIds);
         }
 
         public DeleteServerRequestHandler(IAppDbContext context, IAuthorizedUserProvider userProvider, IRoleRepository roleRepository, IServerProfileRepository serverProfileRepository) : base(context, userProvider)
