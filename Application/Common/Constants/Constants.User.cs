d8a7396b3ef15d0e997bef85a159e9a9dbb253e4
Grabtot <dneshotkin@gmail.com>
2023-11-30 18:17:50 +0200

    fex default avatar image protection

--- Application/Common/Constants/Constants.User.cs
+++ Application/Common/Constants/Constants.User.cs
@@ -12,11 +12,21 @@ namespace Sparkle.Application.Common.Constants
             public static readonly Guid DefaultUser1Id = Guid.Parse("c1ce2c28-9cf4-4ab1-bddb-b99c5408ee34");
 
             public const string Avatar1Id = "656877a14d19836829e23b1e";
-            public const string Avatar2Id = "656877a14d19836829e23b1e";
-            public const string Avatar3Id = "656877a14d19836829e23b1e";
-            public const string Avatar4Id = "656877a14d19836829e23b1e";
-            public const string Avatar5Id = "656877a14d19836829e23b1e";
-            public const string Avatar6Id = "656877a14d19836829e23b1e";
+            public const string Avatar2Id = "656877db4d19836829e23b1f";
+            public const string Avatar3Id = "656877e04d19836829e23b20";
+            public const string Avatar4Id = "656877e54d19836829e23b21";
+            public const string Avatar5Id = "656877ef4d19836829e23b22";
+            public const string Avatar6Id = "656877f34d19836829e23b23";
+
+            public static readonly string[] DefaultAvatarIds = new string[]
+            {
+                Avatar1Id,
+                Avatar2Id,
+                Avatar3Id,
+                Avatar4Id,
+                Avatar5Id,
+                Avatar6Id
+            };
         }
     }
 }
