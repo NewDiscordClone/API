6b80040118b58963727b2bbeaa32de5d85bb301d
Grabtot <dneshotkin@gmail.com>
2023-12-02 16:24:20 +0200

    created stickers command #168

--- Application/Common/Constants/Constants.Message.cs
+++ Application/Common/Constants/Constants.Message.cs
@@ -1,10 +1,52 @@
-﻿namespace Sparkle.Application.Common.Constants
+﻿#pragma warning disable IDE1006 // Naming Styles
+using System.Reflection;
+
+namespace Sparkle.Application.Common.Constants
 {
     public static partial class Constants
     {
         public static class Message
         {
             public const int MaxTextLength = 2000;
+
+            public static class Stickers
+            {
+                public const string Sticker1Id = "656b3377a05b37a590db4d6a";
+                public const string Sticker2Id = "656b3390a05b37a590db4d6b";
+                public const string Sticker3Id = "656b3395a05b37a590db4d6c";
+                public const string Sticker4Id = "656b339ba05b37a590db4d6d";
+                public const string Sticker5Id = "656b33a0a05b37a590db4d6e";
+                public const string Sticker6Id = "656b33a5a05b37a590db4d6f";
+                public const string Sticker7Id = "656b33aba05b37a590db4d70";
+                public const string Sticker8Id = "656b33bba05b37a590db4d72";
+                public const string Sticker9Id = "656b33c0a05b37a590db4d73";
+            }
+
+            public static string[] StickerIds
+            {
+                get
+                {
+                    Type type = typeof(Stickers);
+
+                    List<string?> constants = type.GetFields(BindingFlags.Public |
+                        BindingFlags.Static |
+                        BindingFlags.FlattenHierarchy)
+                        .Where(fi => fi.IsLiteral && !fi.IsInitOnly)
+                        .Select(x => x.GetRawConstantValue()?.ToString())
+                        .ToList();
+
+                    List<string> result = new();
+                    foreach (string? constant in constants)
+                    {
+                        if (constant != null)
+                        {
+                            result.Add(constant);
+                        }
+                    }
+
+                    return result.ToArray();
+                }
+            }
         }
     }
 }
---
+++ Application/Medias/Queries/GetStickers/StickersQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.Extensions.Options;
+using Sparkle.Application.Common.Constants;
+using Sparkle.Application.Common.Options;
+
+namespace Sparkle.Application.Medias.Queries.GetStickers
+{
+    public class StickersQueryHandler : IRequestHandler<StickersQuery, string[]>
+    {
+        private readonly string _mediaUri;
+
+        public Task<string[]> Handle(StickersQuery _, CancellationToken cancellationToken)
+        {
+            string[] stickerIds = Constants.Message.StickerIds;
+
+            string[] stickerUris = new string[stickerIds.Length];
+
+            for (int i = 0; i < stickerIds.Length; i++)
+            {
+                string stickerId = stickerIds[i];
+                string stickerUri = $"{_mediaUri}/{stickerId}.png";
+
+                stickerUris[i] = stickerUri;
+            }
+
+            return Task.FromResult(stickerUris);
+        }
+
+        public StickersQueryHandler(IOptions<ApiOptions> options)
+        {
+            string apiUrl = options.Value.ApiUri;
+            _mediaUri = apiUrl + "/api/media";
+        }
+    }
+}
